4f52db75cfc0a38a5eb2250087a8579ad5eab633
nate131 <nate131@gmail.com>
2023-12-08 00:01:22 +1100

    day 5 brute

--- 2023/5/main_brute.py
+++ 2023/5/main_brute.py
@@ -1,5 +1,10 @@
 import time
+import logging
+import threading
+import time
 import copy
+from tqdm import tqdm
+from multiprocessing import Pool
 start_time = time.time()
 
 #my_file = open("sample.txt", "r") 
@@ -11,7 +16,7 @@ my_file.close()
 
 
 def part_1():
-    print("Part 1:")
+    #print("Part 1:")
     seeds = input_val[0].split(" ")[1:]
     ans = []
     #print(seeds)
@@ -35,15 +40,15 @@ def part_1():
                         current_value = current_value - rule[0] + rule[1]
                         complete = True
             pathway.append(current_value)
-        print(seed, current_value)
-        print(pathway)
+        #print(seed, current_value)
+        #print(pathway)
         ans.append(current_value)
     ans.sort()
-    print(ans[0])
+    print("Part 1:",ans[0])
 
 
-def part_2(starter=0,jump=1):
-    print("Part 2:")
+def part_2(start_num=0,end_num=1,jump=1,id=0):
+    #print("Part 2 Thread Start:",start,end,jump,id)
     seeds = input_val[0].split(" ")[1:]
     ans = []
     #print(seeds)
@@ -59,7 +64,9 @@ def part_2(starter=0,jump=1):
     map_sets.reverse()
     seed_pairs = [seeds[i:i+2] for i in range(0,len(seeds), 2)]
     #print(seed_pairs)
-    for seed in range(starter,100000000000,jump):
+    min_seed = 999999999999999999999
+    #for seed in trange(start_num,end_num,jump,position=id,desc='Thread #{id}'.format(id=id),leave=False):
+    for seed in range(start_num,end_num,jump):
         current_value = copy.copy(int(seed))
         pathway = [current_value]
         for level in range(0,len(input_val[1:])):
@@ -75,11 +82,70 @@ def part_2(starter=0,jump=1):
         #print("Tested",seed, "got",current_value)
         for test in seed_pairs:
             if current_value >= int(test[0]) and current_value <= int(int(test[0])+int(test[1])):
-                print(seed, "WINNER!!")
+                if seed < min_seed:
+                    min_seed = seed
+    return min_seed
                 #print(test)
-                return [seed,jump]
+    #print("Thread",id,"no result")
 
 part_1()
-vals = part_2(0,10000)
-part_2(vals[0]-vals[1],1)
+
+#threads
+min = 99999999999999
+max = 0
+map_sets = [ {'level': x, 'rules':[]} for x in range(0,len(input_val[1:])) ]
+for idx,maps in enumerate(input_val[1:]):
+    #print(maps.split("\n")[1:])
+    for item in maps.split("\n")[1:]:
+        start = int(item.split(" ")[1])
+        end = int(item.split(" ")[0])
+        range_num = int(item.split(" ")[2])
+        if start < min:
+            min=start
+        if (start+range_num) > max:
+            max=start+range_num
+NUM_CORES = 5000
+
+
+chunk_size = round((max-min) / NUM_CORES)
+result = []
+MIN_VALUE=9999999999999999
+PBAR = tqdm(total=NUM_CORES)
+PROCESSES = 14
+CURRENT_COMPLETE = 0
+POOL = Pool(processes=PROCESSES)
+CURRENT_MIN_VALUE_PROCESS=0
+def collect_result(val):
+    global PBAR
+    global MIN_VALUE
+    global CURRENT_COMPLETE
+    global PROCESSES
+    global CURRENT_MIN_VALUE_PROCESS
+    PBAR.update(1)
+    CURRENT_COMPLETE = CURRENT_COMPLETE + 1
+    if val is not None:
+        if val < MIN_VALUE:
+            MIN_VALUE = val
+            CURRENT_MIN_VALUE_PROCESS = CURRENT_COMPLETE
+            print("New Min Value:",MIN_VALUE)
+    if (CURRENT_COMPLETE > CURRENT_MIN_VALUE_PROCESS + PROCESSES) and MIN_VALUE !=9999999999999999:
+        POOL.terminate()
+    return result.append(val)
+def error_result(val):
+    print(f"writing error {val}")
+    return result.append(val)
+result_f = ''
+result_final=[]
+
+for x in range(0,NUM_CORES):
+    #print("Starting Thread:",x)
+    result_f = POOL.apply_async(func=part_2, args=(min+(chunk_size*x),min+(chunk_size*x)+chunk_size,1,x, ), callback=collect_result,error_callback=error_result)
+    result_final.append(result_f)
+POOL.close()
+POOL.join()
+
+#print(results)
+#results_vals = results.values().sort()
+print("Part 2:",MIN_VALUE)
+
 print("--- %s seconds ---" % (time.time() - start_time))
